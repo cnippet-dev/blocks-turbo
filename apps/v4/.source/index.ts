72e7ce17dce34e8de2fe2798ad818d8366cf6103
Deepak Negi <negideepak.in@gmail.com>
2025-11-10 20:29:29 +0530

    chore(dependencies): v4 init, update pnpm-lock.yaml with new package versions and add new dependencies for improved functionality

---
+++ apps/v4/.source/index.ts
@@ -0,0 +1,5 @@
+// @ts-nocheck -- skip type checking
+import * as docs_0 from "../content/docs/components/accordion.mdx?collection=docs"
+import { _runtime } from "fumadocs-mdx/runtime/next"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"components/accordion.mdx","fullPath":"content\\docs\\components\\accordion.mdx"}, data: docs_0 }], [])
