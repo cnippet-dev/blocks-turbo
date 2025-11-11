4e513ee8ddbba5cbb9f612cc40bcdee5c1621d9b
Deepak Negi <negideepak.in@gmail.com>
2025-11-11 21:21:59 +0530

    chore(misc): minor updates

--- apps/v4/next.config.mjs
+++ apps/v4/next.config.mjs
@@ -23,6 +23,18 @@ const nextConfig = {
         protocol: "https",
         hostname: "avatar.vercel.sh",
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
   experimental: {
